fa6d3018749affd3199e48abd09f285d22e9494d
Claudio Novoa <claudio.novoa@inzpiral.com>
2019-11-18 00:20:28 -0300

    Returning serie structured object

--- src/series-scraper.js
+++ src/series-scraper.js
@@ -6,12 +6,16 @@ const url = 'https://www.cntv.cl/videoteca';
 const parseSeries = async () => {
   try {
     const response = await axios.get(url);
-    const dom = new JSDOM(response.data);
-    const series = dom.window.document.querySelectorAll('.info > a');
+    const dom = JSDOM.fragment(response.data);
+    const items = Array.from(dom.querySelectorAll('.item'));
    
-    for (serie of series) {
-      console.log('666', serie.href);
-    }
+    const series = items.map((serie) => {
+      const url = serie.querySelector('a').href;
+      const img = serie.querySelector('img').src;
+      const title = serie.querySelector('h2').textContent;
+
+      return { url, img, title }
+    });
     return series;
   } catch(error) {
     console.error(error)
@@ -22,7 +26,7 @@ const parseSeries = async () => {
 (async () => {
     try {
       const series = await parseSeries(url);
-      // console.log('series', series);
+      console.log('series', series);
     } catch (e) {
       console.error('Something went wrong!');
     }
